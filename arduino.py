5aabfb62014c19831e547e6a4beaa93f0ca0b698
hamzaplojovic <hamzaplojovic4@gmail.com>
2022-08-20 12:44:47 +0200

    pyw

--- arduino.py
+++ arduino.py
@@ -2,13 +2,13 @@ import serial
 import time
 import httpx
 
-ser = serial.Serial('/dev/cu.usbmodem11201', 9600)
+ser = serial.Serial('/dev/cu.usbmodem1201', 9600)
 time.sleep(1)
 
 while True:
     line = ser.readline().decode("utf-8")
     if len(line.strip()) > 0:
         httpx.get(
-            f"https://lrlegx.deta.dev/temperature/add?new={line.strip()[41:-1]}")
+            f"https://lrlegx.deta.dev/temperature/add?new={str(line.strip()[41:-1])}")
         httpx.get(
             f"https://lrlegx.deta.dev/humidity/add?new={line.strip()[19:24]}")
